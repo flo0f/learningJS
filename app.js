597732dbe72d65be0372a9ebef970962d1256cd6
flo0f <144050720+flo0f@users.noreply.github.com>
2023-09-07 22:07:06 +0500

    Операторы присваивания и сравнения

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-// Арифметические операторы
-const width = 10;
-const height = 5;
-const space = width * height;
-const newWidth = width + 4;
-const newWidth2 = width - 4;
-const division = newWidth / newWidth2
-const volume = 2 ** 3; // 2 * 2 * 2
-console.log(volume)
-
-// Строки
-const city = 'Moscow';
-const street = 'Pushkina';
-console.log(city + ', ' + street + ' 5')
 // Операторы присваивания
-let age = 18;
+let age = 18; // равно это оператор присваивания
 age += 2; // age = age + 2
+age -= 3; // age = age - 3
+age *= 2; // age = age * 2
+age /= 2; // age = age / 2
+
+age++; // age = age + 1
+age--; // age = age - 1
 
 console.log(age);
+
+// Операторы сравнения
+const driverLicense = 18;
+console.log(age > driverLicense); // левое больше?
+console.log(age >= driverLicense); // левое больше или равно?
+console.log(age < driverLicense); // левое меньше?
+console.log(age <= driverLicense); // левое меньше или равно? 
+console.log(age == driverLicense); // равно ли левое правому?
