fbb37b179fda3c9ee31ac5ffcb43909aee1f15d2
flo0f <144050720+flo0f@users.noreply.github.com>
2023-09-07 21:59:10 +0500

    Урок 12 Базовые операторы

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('hello world')
+// Арифметические операторы
+const width = 10;
+const height = 5;
+const space = width * height;
+const newWidth = width + 4;
+const newWidth2 = width - 4;
+const division = newWidth / newWidth2
+const volume = 2 ** 3; // 2 * 2 * 2
+console.log(volume)
+
+// Строки
+const city = 'Moscow';
+const street = 'Pushkina';
+console.log(city + ', ' + street + ' 5')
+// Операторы присваивания
+let age = 18;
+age += 2; // age = age + 2
+
+console.log(age);
