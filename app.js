0f57cd3e3f5dc0c68e48f7409f52d0868ccdb53d
flo0f <144050720+flo0f@users.noreply.github.com>
2023-09-08 21:50:17 +0500

    Ур18 Преобразование типов

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const projectName = 'Сайт магазина';
-const price = 2000;
-const author = 'Василий Петров';
-// строка созданная с помощью конкатенации
-const template = author + ' заказал ' + projectName + ' по цене ' + price;
-console.log(template);
-// шаблонная строка СОЗДАЕТСЯ С ПОМОЩЬЮ КОСЫХ КАВЫЧЕК ``
-const template2 = `${author} заказал ${projectName} по цене ${price}$`;
-console.log(template2);
+// Конвертация в число, обязательно Number, не number
+const age = '18';
+console.log(Number(age) + 5);
+console.log(age - 3); // '-' '/' '*' не требует Number()
 
-// Перенос строки без шаблонной строки
-const template3 = 'Проект ' + projectName + '\n' + 'Цена ' + price + '$';
-console.log(template3);
-// Перенос строки с помощью шаблонной строки
-const template4 = `Проект ${projectName}
-Цена ${price}$`;
-console.log(template4);
+const userName = 'Вася';
+console.log(Number(userName) + 5); // NaN = Not a Number
+console.log(userName - 5); // Также NaN
+console.log(typeof NaN); // number
+
+// Конвертация в строки String()
+console.log(String(4) + 7);
+
+// Конвертация в Булевое значение(true/false) Boolean()
+console.log(Boolean(1)); // true любое число кроме нуля будет истина
+console.log(Boolean(0)); // false нуль будет ложь
+console.log(Boolean(' ')); // true т.к. в строке есть хотя бы один символ
+console.log(Boolean('')); // false т.к. строка пустая
+// Булевое значение можно использовать как число
+console.log(Boolean('') + 10); // =10 булевый false = 0
+console.log(Boolean('') + 10); // =11 булевый true = 1
