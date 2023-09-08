ab3c06a7d9bc1ddcb94973fee76348eaae004fde
flo0f <144050720+flo0f@users.noreply.github.com>
2023-09-08 21:29:22 +0500

    Ур17 шаблонные строки

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
-/*
-Ваша часовая ставка 80$ и вы готовы работать не
-более 5 часов в день 5 дней в неделю (кроме выходных).
-К вам приходит заказчик и предлагает заказ на 40
-часов работы.
-Сейчас понедельник.
-Вы должны уехать через 11 дней.
-Выведете в консоль:
-- Boolean переменную успеете ли вы взяться за работу
-- Сколько вы за неё попросите?
-*/
-// Данные
-const timePerDay = 5;
-const daysPerWeek = 5;
-const orderTime = 40;
-const timeLeft = 11;
-const salaryPerHour = 80;
+const projectName = 'Сайт магазина';
+const price = 2000;
+const author = 'Василий Петров';
+// строка созданная с помощью конкатенации
+const template = author + ' заказал ' + projectName + ' по цене ' + price;
+console.log(template);
+// шаблонная строка СОЗДАЕТСЯ С ПОМОЩЬЮ КОСЫХ КАВЫЧЕК ``
+const template2 = `${author} заказал ${projectName} по цене ${price}$`;
+console.log(template2);
 
-// Расчеты
-const orderCost = orderTime * salaryPerHour;
-const workDays = timeLeft - 2;
-const hoursCanWork = workDays * timePerDay;
-
-// Вывод в консоль
-console.log('Смогу ли я работать' + ' ' + (orderTime <= hoursCanWork));
-console.log('Цена заказа' + ' ' + orderCost + 'usd');
+// Перенос строки без шаблонной строки
+const template3 = 'Проект ' + projectName + '\n' + 'Цена ' + price + '$';
+console.log(template3);
+// Перенос строки с помощью шаблонной строки
+const template4 = `Проект ${projectName}
+Цена ${price}$`;
+console.log(template4);
