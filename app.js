4590ae63a82dcac9e79cbde22d30a5226f51ba43
flo0f <144050720+flo0f@users.noreply.github.com>
2023-09-14 20:19:17 +0500

    Ур31 Введение в функции

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 /*
-	Пользователь хочет приобрести игру в магазине
-	Он может это сделать отлько если:
-	- Eго баланс больше 1000 (balance) 
-	или число бонусов больше 100 (bonusBalance)
-	- Он не забанен (isBanned)
-	- Игра не кулена (isExist)
-	- Игра в продаже (isSelling)
-	Напишите условие для покупки и выведите в консоль
-	результат
+function - ключевое слово инициализации функции
+после function идет название функции
+в скобках после названия - аргументы функции
+return - возвращает значение из функции
 */
-const user = 'gamer1337';
-const balance = 1200;
-const bonusBalance = 90;
-const isBanned = false;
+let myName = 'anton'
+function logName(name, surname) {
+	console.log(`Мое имя ${name} ${surname}`);
+}
+logName(myName, 'pupkin'); // вызов функции
 
-const isBought = false;
-const isSelling = true;
+function deposit(depositInUSD, month, rate) {
+	return depositInUSD * (1 + rate / 12) ** month;
+}
 
-const canBuy = (balance > 1000 || bonusBalance > 100) && !isBanned && !isBought && isSelling;
-
-console.log(`Пользователь ${user} сможет купить игру?
-Ответ: ${canBuy ? 'ДА' : 'НЕТ'}`);
+const example1 = deposit(5000, 12, 0.20);
+console.log(example1);
