8f288956d41a254011330e55ddf46c42b3d663b7
flo0f <144050720+flo0f@users.noreply.github.com>
2023-09-17 19:30:57 +0500

    Ур43 Из строки в массив и обратно

--- app.js
+++ app.js
@@ -1,48 +1,10 @@
-const roles = ['user', 'admin', 'manager', 'superUser'];
+const roles = ['user', 'admin', 'manager', 'superuser'];
 
-// .slice() позволяет взять СРЕЗ массива, т.е. от N(начальный элемент) до M(конечный элемент) индекса
-// .slice() не модифицирует исходный массив
-// если указать 1 аргумент, то он возьмет элементы от этого индекса и до конца
-const res = roles.slice(2);
-console.log(roles);
+// .split() разбивает строку по знаку из () возвращая массив
+const url = 'auth/user/login';
+const res = url.split('/');
 console.log(res);
-// если указать 2 аргумента, то будут взяты все элементы от N включительно, до M не включительно
-const res2 = roles.slice(2, 3);
-console.log(roles);
-console.log(res2);
-// можно брать элементы с конца указывая отрицательные индексы, например индекс -2 возьмет последние два элемента
-const res3 = roles.slice(-2);
-console.log(roles);
-console.log(res3);
 
-const res4 = roles.slice(1, -1);
-console.log(roles);
-console.log(res4);
-
-console.log('---===---');
-// .splice() схож с .slice(), НО он изменяет исходный массив
-// также .splice() берёт элементы от N, M элементов
-// он УДАЛЯЕТ часть массива, возвращая при этом удаленные элементы в виде массива
-// если передать только один аргумент, то будут вырезаны элементы с этого индекса и до конца массива
-console.log(roles);
-const res5 = roles.splice(2);
-console.log(res5);
-console.log(roles);
-
-roles.push('moderator', 'premium');
-console.log(roles);
-const res6 = roles.splice(2, 1); // беру с 2го индекса один элемент
-console.log('===---===---===');
-console.log(roles);
-console.log(res6);
-// .splice(-1) удаляет последний элемент массива
-
-// .reverse() разворачивает массив, при этом модифицируя его
-const res7 = roles.reverse();
-console.log(res7);
-
-// конкатенация массивов
-const newRoles = ['sysadmin', 'developer'];
-const res8 = roles.concat(newRoles);
-// X.concat(Y) соединяет массив Х с массивом Y при этом в начале идут элементы массива X
-console.log(res8);
+// преобразование массива в строку
+// .join() объединяет элементы массива в строку, знак из () является разделителем
+console.log(roles.join('='));
