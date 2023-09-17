189970afab084ed405fe34ca325d2f508935b523
flo0f <144050720+flo0f@users.noreply.github.com>
2023-09-17 19:57:31 +0500

    Ур44 Упражнение - обновление списка задач

--- app.js
+++ app.js
@@ -1,10 +1,47 @@
-const roles = ['user', 'admin', 'manager', 'superuser'];
+/*
+    Дан список задач
+    const tasks = ['Задача 1'];
+    Сделать функции:
+    - Добавление задачи в конец
+    - Удаление задачи по названию
+    - Перенос задачи в начало списка по названию
+    Всегда меняем исходный массив
+*/
 
-// .split() разбивает строку по знаку из () возвращая массив
-const url = 'auth/user/login';
-const res = url.split('/');
-console.log(res);
+const tasks = ['Задача 1'];
 
-// преобразование массива в строку
-// .join() объединяет элементы массива в строку, знак из () является разделителем
-console.log(roles.join('='));
+const addTask = (mas, taskName) => mas.push(taskName);
+const delTask = (mas, taskName) => {
+    if (mas.includes(taskName)) {
+        mas.splice(mas.indexOf(taskName), 1);
+        return `Элемент "${taskName}" успешно удален!`;
+    } else {
+        return `Элемента с именем "${taskName}" не существует!`;
+    }
+}
+const moveTask = (mas, taskName) => {
+    if (mas.includes(taskName)) {
+        const element = mas.splice(mas.indexOf(taskName), 1);
+        mas.unshift(element[0]);
+        return `Элемент "${taskName}" успешно перенесён!`;
+    } else {
+        return `Элемента с именем "${taskName}" не существует!`;
+    }
+}
+addTask(tasks, 'Задача 2');
+console.log(tasks);
+addTask(tasks, 'Задача 3');
+console.log(tasks);
+addTask(tasks, 'Задача 4');
+console.log(tasks);
+
+console.log(delTask(tasks, 'Задача 1'));
+console.log(tasks);
+
+console.log(moveTask(tasks, 'Задача 4'));
+console.log(tasks);
+
+console.log(moveTask(tasks, 'Задача 7-8'));
+console.log(tasks);
+
+
