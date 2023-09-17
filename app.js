b22d04fa97e060ba7144d81494d872c6690b056d
flo0f <144050720+flo0f@users.noreply.github.com>
2023-09-17 20:42:37 +0500

    Ур48 Упражнение - Функция разбора URL

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
-// rest оператор = ...
+/*
+    Дан произвольный url -
+    'https://purpleschool.ru/course/javascript'
+    Нужно сделать функцию, которая выводит в консоль:
+    - Протокол (https)
+    - Доменное имя (purpleschool.ru)
+    - Путь внутри сайта (/course/javascript)
+*/
 
-const data = [1, 2, 3, 4 ,5 ,6];
-const [one, two, ...others] = data;
-console.log(one, two, others);
-// rest оператор нельзя использовать нигде кроме последней переменной
-// const [one, ...others, six] = data; => ошибка
+const url = 'https://purpleschool.ru/course/javascript';
+/*
+МОЯ РЕАЛИЗАЦИЯ
+
+function getUrlPart(url) {
+    const [protocol, _, host, ...path] = url.split('/');
+    console.log(`Протокол: ${protocol.slice(0, -1)}.
+Домен: ${host}
+Путь: ${'/' + path.join('/')}`)
+}
+
+*/
+
+// реализация из видео
+function getUrlParts(url) {
+    const [protocol, _, host, ...path] = url.split('/');
+    if (protocol === 'https:' || protocol === 'http:') {
+        if (!host.includes('.')) {
+            return;
+        }
+        console.log(protocol, host, path);
+        console.log(`Протокол: ${protocol.split(':')[0]}`);
+        console.log(`Доменное имя: ${host}`);
+        console.log(`Путь внутри сайта: /${path.join('/')}`);
+    }
+}
+
+getUrlParts(url);
