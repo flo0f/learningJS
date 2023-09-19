c5ccb6eb39f9230029cafcda536770f1156f3ed7
flo0f <144050720+flo0f@users.noreply.github.com>
2023-09-19 20:22:37 +0500

    Ур54 Циклы for of и for in

--- app.js
+++ app.js
@@ -1,35 +1,19 @@
-// запись цикла через for
-for (let i = 1; i < 5; i++) {
-    console.log(`Вывод for - ${i}`);
-}
-
-// запись цикла через while
-let i = 1;
-while (i < 5) {
-    console.log(`Вывод while - ${i}`);
-    i++;
-}
-console.log('---===---===---===---')
-const arr = [1, 3, 4, 5, 6, 8, 9];
+const arr = [1, 4, 7, 8];
 
 for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 5) {
-        break;
-    }
     console.log(arr[i]);
 }
 
-// тот же цикл но через while
-let j = 0;
-while (arr[j] <= 5 && j < arr.length) {
-    console.log(arr[j]);
-    j++;
+console.log('---===---===---===---');
+
+// for of - упрощение обхода массива
+for (let element of arr) {
+    console.log(element);
 }
 
-// конструкция do while
-let k = 0;
-do {
-    console.log(k);
-    k++;
-} while (k < 0);
-// в этой конструкции проверка происходит после итерации цикла, а не до него как в случае for или while
+console.log('---===---===---===---');
+
+// for in - перебор массива по индексу
+for (let index in arr) {
+    console.log(arr[index]);
+}
