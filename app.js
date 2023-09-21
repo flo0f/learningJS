d6d5a1bd848e1700e028438869d66ac27a43a622
flo0f <144050720+flo0f@users.noreply.github.com>
2023-09-21 20:08:17 +0500

    Ур56 Функции высшего порядка

--- app.js
+++ app.js
@@ -1,51 +1,10 @@
-/*
-    Есть выгрузка операций пользователя
-    const operations = [1000, -700, 300, -500, 10000];
-    а так же начальный баланс в 100$
-    Необходимо сделать функции расчета:
-    - Итогового баланса
-    - Наличия отрицательного баланса (если после очередной операции
-        баланс < 0, то выдавать false)
-    - Расчета среднего расхода и дохода
-*/
-const operations = [1000, -700, 300, -500, 10000];
-const balance = 100;
-// Итоговый баланс
-function totalBalance(bal = 0) {
-    for (let i = 0; i < operations.length; i++) {
-        bal += operations[i];
-    }
-    return bal;
-}
-console.log(totalBalance(balance));
-// Был ли отрицательный баланс
-function belowZero(bal = 0) {
-    for (let index in operations) {
-        bal += operations[index];
-        if (bal < 0) {
-            return false;
-        }
-    }
-    return true;
-}
-console.log(belowZero(balance));
-// Средний расчет и доход
+// функции высшего порядка принимают и/или возвращают другие функции
+const a = (b) => b++;
 
-function average(bal = 0) {
-    let income = 0;
-    let waste = 0;
-    let i = 0;
-    let j = 0;
-    for (let element of operations) {
-        if (element > 0) {
-            income += element;
-            i++;
-        } else {
-            waste += element;
-            j++;
-        }
-    }
-    return `Средний доход: ${income / i}
-Средние траты: ${waste / j}`;
+function g(a) { // принимает другие функции
+
 }
-console.log(average(balance));
+
+function f() { // возвращает другие функции
+    return a;
+}
