2cc11a0b1855c831a285e84305bf8395fa9a7daf
flo0f <144050720+flo0f@users.noreply.github.com>
2023-09-22 21:47:51 +0500

    Ур65 Упражнение - среднее значение

--- app.js
+++ app.js
@@ -1,35 +1,25 @@
-// reduce - сужает массив до конкретного значения
-const operations = [100, -20, 7, -30, 50];
+/*
+    Найти среднее значение последовательности
+    чисел с помощью reduce
+*/
+const arr = [2, 4, 4, 10];
 
-// // реализация обычная
-// let balance = 0;
-// for (const operation of operations) {
-//     balance += operation;
-// }
-// console.log(balance);
+// // моя реализация
+// const avgArr = arr.reduce((prev, current, i) => {
+//     if (arr[arr.length - 1] == current) {
+//         return (prev + current) / arr.length;
+//     } else {
+//         return prev + current
+//     }
+// }, 0);
+// console.log(avgArr);
 
-// через метод .reduce()
-const finalBalance = operations.reduce((acc, value, i) => {    // acc = accumulator
-    console.log(`Итерация ${i}. Аккумулятор = ${acc}, следующее значение ${value}`)
-    return acc += value;
+// реализация из видео 
+const avg = arr.reduce((acc, el, i) => {
+    if (i != arr.length - 1) {
+        return acc + el;
+    } else {
+        return (acc + el) / arr.length;
+    }
 }, 0);
-console.log(finalBalance);
-// iter 0 - acc = 0, value = 100
-// iter 1 - acc = будет равен тому, что возвращено из функции, т.е. 100, value = -20
-// iter 2 - acc = 80, value = 7
-// iter 3 - acc = 87, value = -30
-// iter 4 - acc = 57, value = 50
-// iter 5 - acc = 107, value = undefined
-
-// найдем минимальный элемент из списка
-const numbers = [1, 4, 6, 7, 4, 3, 9, -5];
-
-// первый аргумент - функция, второй аргумент - значение, которое заменяется при выполнении условия
-const minElement = numbers.reduce((accumulator, num) => {   // .reduce((предыдущееЗначение, следующееЗначение))
-    if (num > accumulator) {                                // если следующее больше предыдущего, то оставляем пред.
-        return accumulator;
-    } else {                                                // иначе предыдущее стает следующем
-        return num;
-    } 
-}, 0); 
-console.log(minElement);
+console.log(avg);
