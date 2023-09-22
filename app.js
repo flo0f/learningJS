01c9c4d24209e8eea31251130578ab119003bf7d
flo0f <144050720+flo0f@users.noreply.github.com>
2023-09-22 21:34:55 +0500

    Ур64 reduce

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
-/*
-    Имеется массив изменения цен prices, где внутри
-    1й элемент массива является ценой в момент X,
-    2Й - ценой в момент Y.
-    Нужно преобразовать данные в массив, где будут отображены только положительные изменения цен: [100, 150]
-*/
+// reduce - сужает массив до конкретного значения
+const operations = [100, -20, 7, -30, 50];
 
-const prices = [[100, 200], [120, 100], [200, 350]];
+// // реализация обычная
+// let balance = 0;
+// for (const operation of operations) {
+//     balance += operation;
+// }
+// console.log(balance);
 
-// моя реализация
-const positiveChanges = prices
-    .map((pricesEl, i) => (pricesEl[1] - pricesEl[0]))
-    .filter((priceElAfterMap) => priceElAfterMap > 0);
+// через метод .reduce()
+const finalBalance = operations.reduce((acc, value, i) => {    // acc = accumulator
+    console.log(`Итерация ${i}. Аккумулятор = ${acc}, следующее значение ${value}`)
+    return acc += value;
+}, 0);
+console.log(finalBalance);
+// iter 0 - acc = 0, value = 100
+// iter 1 - acc = будет равен тому, что возвращено из функции, т.е. 100, value = -20
+// iter 2 - acc = 80, value = 7
+// iter 3 - acc = 87, value = -30
+// iter 4 - acc = 57, value = 50
+// iter 5 - acc = 107, value = undefined
 
-console.log(positiveChanges);
+// найдем минимальный элемент из списка
+const numbers = [1, 4, 6, 7, 4, 3, 9, -5];
 
-// реализация из ролика
-const result = prices
-    .map(product => product[1] - product[0])
-    .filter(price => price > 0);
-
-console.log(result);
+// первый аргумент - функция, второй аргумент - значение, которое заменяется при выполнении условия
+const minElement = numbers.reduce((accumulator, num) => {   // .reduce((предыдущееЗначение, следующееЗначение))
+    if (num > accumulator) {                                // если следующее больше предыдущего, то оставляем пред.
+        return accumulator;
+    } else {                                                // иначе предыдущее стает следующем
+        return num;
+    } 
+}, 0); 
+console.log(minElement);
