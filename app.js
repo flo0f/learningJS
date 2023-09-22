ee318244a5b7c70d2d2a8d8b1b81ec84e3289b07
flo0f <144050720+flo0f@users.noreply.github.com>
2023-09-22 20:51:25 +0500

    Ур61 map

--- app.js
+++ app.js
@@ -1,37 +1,21 @@
-
-const score = [5, 10, 0, 15];
-
-// обычный способ итерации по массиву
-for (const el of score) {
-    console.log(`Очки за ${score.indexOf(el) + 1} раунд: ${el}`);
+const transactionsInUSD = [10, -7, 50, -10, 100];
+// процедурный стиль написания
+const transactionsInRUB = [];
+for (const transactions of transactionsInUSD) {
+    transactionsInRUB.push(transactions * 60);
 }
-// метод .entries возвращает индекс и сам элемент
-for (const [i, el] of score.entries()) {
-    console.log(`Раунд ${i + 1}: ${el}`);
-}
+
+console.log(transactionsInRUB);
+console.log(transactionsInUSD);
+
+// функциональный стиль
+// .map() принимает в аргументы то же, что и .forEach(), а именно
+// .map(callbackFn (value, index, array))
+// .map() в отличии от forEach возвращает массив с элементами исходного к которым применена функция из (...) map'a
+const transactionsInRUB2 = transactionsInUSD
+    .map((transactions) => transactions * 60); // методы можно переносить
 console.log('---===---===---===---');
-// метод forEach
-// X.forEach() принимает функцию, которая будет применена к каждому элементу массива X
+console.log(transactionsInRUB2);
+console.log(transactionsInUSD);
 
-// score.forEach(function (el) {
-//     console.log(`Очки ${el}`)   // с анонимной функцией
-// })
 
-// score.forEach((el) => {
-//     console.log(`Очки ${el}`)  // со стрелочной функцией
-// })
-
-// const iterator = (el) => {
-//     console.log(`Очки ${el}`);   // с отдельно вынесенной функцией
-// }
-// score.forEach(iterator);
-
-// вторым аргументом функции можно получить индекс элемента массива
-// score.forEach((el, i) => {
-//     console.log(`Очки за ${i + 1} раунд: ${el}`)
-// });
-
-// в .forEach() можно передать и третий аргумент, он будет являтся итерируемым массивом
-score.forEach((scoreEl, i, arr) => {
-    console.log(`Очки за ${i + 1} раунд: ${scoreEl}, весь список очков ${arr}`);
-});
