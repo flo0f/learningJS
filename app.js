9c82f2c03946def938db6839dfc96aad6400af28
flo0f <144050720+flo0f@users.noreply.github.com>
2023-09-23 18:46:51 +0500

    Ур68 flat и flatMap

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
-/*
-    Написать функцию, которая возвращает true,
-    если элемент есть, и false, если нет.
-*/
-const arr = [2, 4, 4, 10, 20];
+const prices = [[2, 4], [3, 4], [10, [20, 30]]];
 
-// // моя реализация
-// function some(elToFind, array) {
-//     return array.findIndex(el => el === elToFind) < 0 ? false : true;
-// }
-// console.log(some(5, arr));
+// .flat() делает массив плоским, т.е. убирает вложенность
+// .flat() принимает значение глубины, которое по умолчаиню === 1
+const res = prices.flat(2);
+console.log(res);
 
-// // реализация из видео
-// function some(array, element) {
-//     const res = array.find(el => el === element);
-//     return res == undefined ? false : true;
-// }
-// console.log(some(arr, 2))
-
-// метод .some()
-console.log(arr.some(el => el === 2))
+// .flatMap() последовательное применение .map и .flat
+const res2 = prices.flatMap((el) => el.concat([1]));
+console.log(res2);
