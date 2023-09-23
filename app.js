b7a9cc90bd238a93357579b9cde8c8575543daa9
flo0f <144050720+flo0f@users.noreply.github.com>
2023-09-23 20:51:01 +0500

    Ур69 sort

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
-const prices = [[2, 4], [3, 4], [10, [20, 30]]];
+const users = ['Вася', 'Маша', 'Катя', 'Аня'];
+console.log(users);
 
-// .flat() делает массив плоским, т.е. убирает вложенность
-// .flat() принимает значение глубины, которое по умолчаиню === 1
-const res = prices.flat(2);
-console.log(res);
+// при .sort() массив модифицируется, а .map и .filter возвращают новый массив
+// .sort() без аргументов сортирует по алфавиту как строки
+users.sort();
+console.log(users);
 
-// .flatMap() последовательное применение .map и .flat
-const res2 = prices.flatMap((el) => el.concat([1]));
-console.log(res2);
+// тут произойдет сортировка КАК СТРОК, т.е. сначала символы, потом цифры по возрастанию
+// поэтому сначала будет идти -100, а потом -300
+const operations = [100, -300, -100, 50, 480];
+console.log(operations);
+operations.sort();
+console.log(operations);
+
+// X < 0 - a, b - если вернем, что-то меньше нуля, то порядок сохранен
+// X > 0 - b, a - меняем порядок
+
+// сортировка по возрастанию
+operations.sort((a, b) => {
+    if (a > b) {
+        return 1;
+    } 
+    if (a < b) {
+        return -1;
+    }
+});
+console.log(operations);
+
+// сортировка по убыванию
+operations.sort((a, b) => {
+    if (a < b) {
+        return 1;
+    } 
+    if (a > b) {
+        return -1;
+    }
+});
+console.log(operations);
+
+// самая краткая запись сортировки по возрастанию
+console.log('---===---===---===---');
+operations.sort((a, b) => a - b); // a - b = <0 - сохраняем порядок
+console.log(operations);          // a - b = >0 - меняем порядок
