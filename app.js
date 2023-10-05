766c3eac6b450b93ec19dc4caa9e2a86dbf85664
flo0f <144050720+flo0f@users.noreply.github.com>
2023-10-05 20:56:30 +0500

    Ур75 Упражнение - проверка номера

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
-const str = 'Вася ПуПкИн';
-console.log(str);
-// изменение размера текста
-console.log(str.toUpperCase()); // ВСЕ ПРОПИСНЫЕ
-console.log(str.toLowerCase()); // все строчные
+/* проверить является ли это номером телефона России */
+/* верные */
+const num1 = '89103235356';
+const num2 = '+79103235356';
+const num3 = '+7(910)3235356';
+const num4 = '+7(910) 323-53-56';
+const num5 = '  +7(910) 323-53-56  ';
+/* не верные */
+const num1Error = '89103235';
+const num2Error = '+7d910d323-53-56';
+const num3Error = '9+7103235356';
+const num4Error = '89103g35356';
 
-// замена чего-то в строке
-console.log(str.replace('Вася', 'Олег'));
-console.log(str.replaceAll('П', 'З'));
-
-const str2 = ' Вася Пупкин     \n';
-// удаление лишних пробелов из строки
-console.log(str2.trim()); // удаляет с начала и конца
-console.log(str2.trimEnd()); // удаляет с конца
-console.log(str2.trimStart()); // удаляет с начала
+function validNumber (num) {
+    let x = num.trim();
+    while (x.includes('+')) {
+        x = x.slice(x.indexOf('+') + 1);   
+    } 
+    while (x.includes('(')) {
+        x = x.slice(0, x.indexOf('(')) + x.slice(x.indexOf('(') + 1);
+    }
+    while (x.includes(')')) {
+        x = x.slice(0, x.indexOf(')')) + x.slice(x.indexOf(')') + 1);
+    }
+    while (x.includes(' ')) {
+        x = x.slice(0, x.indexOf(' ')) + x.slice(x.indexOf(' ') + 1);
+    }
+    while (x.includes('-')) {
+        x = x.slice(0, x.indexOf('-')) + x.slice(x.indexOf('-') + 1);
+    }
+    if (x.length != 11 || isNaN(Number(x)) || !(x.startsWith('8') || x.startsWith('7'))) {
+        return `Номер ${num.trim()} не верный :(`
+    } else {
+        return `Номер ${num.trim()} верный! :)`;
+    }
+}
+// true
+console.log(validNumber(num1));
+console.log(validNumber(num2));
+console.log(validNumber(num3));
+console.log(validNumber(num4));
+console.log(validNumber(num5));
+// false
+console.log(validNumber(num1Error));
+console.log(validNumber(num2Error));
+console.log(validNumber(num3Error));
+console.log(validNumber(num4Error));
