47208a327fa63811ef82165cf6fd37773b5c1428
flo0f <144050720+flo0f@users.noreply.github.com>
2023-10-05 20:22:26 +0500

    Ур74 Преобразование строки

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-/* вытащить имя и фамилию в отдельные переменные */
-const userName = 'Вася aka Terminator Пупкин';
-const firstName = userName.slice(0, 4);
-const surname = userName.slice(-6);
-console.log(firstName, surname);
+const str = 'Вася ПуПкИн';
+console.log(str);
+// изменение размера текста
+console.log(str.toUpperCase()); // ВСЕ ПРОПИСНЫЕ
+console.log(str.toLowerCase()); // все строчные
 
-// реализация из урока
-const firstName1 = userName.slice(0, userName.indexOf(' '));
-const surname1 = userName.slice(userName.lastIndexOf(' ') + 1, userName.length);
-console.log(firstName1, surname1);
+// замена чего-то в строке
+console.log(str.replace('Вася', 'Олег'));
+console.log(str.replaceAll('П', 'З'));
+
+const str2 = ' Вася Пупкин     \n';
+// удаление лишних пробелов из строки
+console.log(str2.trim()); // удаляет с начала и конца
+console.log(str2.trimEnd()); // удаляет с конца
+console.log(str2.trimStart()); // удаляет с начала
