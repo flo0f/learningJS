aac9ef44aa7d66fabba251f62396e22ba36e84f5
flo0f <144050720+flo0f@users.noreply.github.com>
2023-10-08 20:23:11 +0500

    Ур80 Обращение к элементам

--- app.js
+++ app.js
@@ -7,5 +7,24 @@ const user = {
         'Code',
         'cooking'
     ],
-    1: 1
+    educationBasic: 'School 45',
+    educationProgressive: 'Harvard'
 };
+
+// member access notation
+console.log(user);
+console.log(user.name);
+console.log(user.skills[0]);
+
+console.log(user['name']);
+const level = 'Basic';
+console.log(user['education' + level]); // При таком обращении к свойству объекта можно проводить рассчеты
+
+// задать свойство объекта
+user['city'] = 'Moscow';
+console.log(user.city);
+
+// модификация существующего свойства
+user.age = 30; // user['age'] = 30;
+console.log(user);
+console.log(user.age);
