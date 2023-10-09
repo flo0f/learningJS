eec4d2aa9483b60803d0186437015b8e5ef4b208
flo0f <144050720+flo0f@users.noreply.github.com>
2023-10-09 18:15:27 +0500

    Ур83 Методы объектов

--- app.js
+++ app.js
@@ -1,37 +1,9 @@
-/*
-    преобразовать пользователей до вида
-    { fullName: 'Вася Пупкин', skillNum: 2}
-
-*/
-
-const users = [
-    {
-        name: 'Вася',
-        surname: 'Пупкин',
-        age: 30,
-        skills: ['Разработка', 'DevOps']
-    },
-    {
-        name: 'Катя',
-        surname: 'Белова',
-        age: 18,
-        skills: ['Дизайн']
+const user = {
+    name: 'Вася',
+    surname: 'Пупкин',
+    age: 24,
+    getFullName: function() {
+        return this.name + ' ' + this.surname;
     }
-];
-const res = users.map((a, i) => {
-    return {
-        fullName: a.name + ' ' + a.surname,
-        skillNum: a.skills.length
-    }
-});
-console.log(res)
-
-// из урока
-const userData = users.map(user => {
-    return {
-        fullName: `${user.name} ${user.surname}`,
-        skillNum: user.skills.length
-    };
-})
-console.log('---');
-console.log(userData);
+};
+console.log(user.getFullName());
