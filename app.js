3eb2f6587a1182171e44cb08e01cc2a958b5d7a1
flo0f <144050720+flo0f@users.noreply.github.com>
2023-10-09 18:32:54 +0500

    Ур84 Упражнение - кошелек

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-const user = {
-    name: 'Вася',
-    surname: 'Пупкин',
-    age: 24,
-    getFullName: function() {
-        return this.name + ' ' + this.surname;
+/*
+    Реализовать методы увеличения и уменьшения баланса,
+    при котором каждая операция сохраняется в массив
+    operations в виде { reason: 'Оплата налогов', sum: -100 }.
+    Возвращается true, если успешно и false, если не хватает баланса
+    Так же реализовать метод вывода числа операций по кошельку
+*/
+
+const wallet = {
+    balance: 0,
+    operations: [],
+    newOperation: function(reason = 'Причина не указана', sum) {
+        newBalance = this.balance += sum;
+        if (newBalance >= 0) {
+            this.balance = newBalance;
+            this.operations.push({ reason: String(reason), sum: sum});
+            return true;
+        } else {
+            return false;
+        }
+    },
+    howManyOperations: function() {
+        return this.operations.length;
     }
-};
-console.log(user.getFullName());
+}
+console.log(wallet.newOperation('Зарплата', 200));
+console.log(wallet.newOperation('Потратил на игру', -15));
+console.log(wallet.newOperation('Купил продукты', -50));
+console.log(wallet.howManyOperations());
+console.log(wallet.operations);
+console.log(wallet.balance)
