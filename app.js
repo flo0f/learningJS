9a50b47aae868cd2d3c3f5dffca52a649dc69e99
flo0f <144050720+flo0f@users.noreply.github.com>
2023-10-12 19:34:45 +0500

    Ур89 Упражнение - склад

--- app.js
+++ app.js
@@ -1,17 +1,56 @@
-const cities = {
-    msk: {
-        temp: {
-            celsius: 25
+/*
+	Сделать объект склад с методами добавления на склад, поиска
+	по складку товара и расчёт веса
+*/
+const warehouse = {
+    goods: [],
+    findGoodById: function (goodId) {
+        return this.goods.find(g => g.id == goodId);
+        },
+    addGood: function (good) {
+        const existedGood = this.findGoodById(good.id);
+        if (existedGood) {
+            return 'Товар уже существет';
         }
+        this['goods'].push(good);
+        return 'Товар успешно добавлен';
     },
-    spb: {
-
+    getWeightKg: function () {
+        return this.goods.reduce((acc, el) => {
+            current = el.weight?.kg;
+            if (current != undefined) {
+                acc += current;
+            } else {
+                acc += 0
+            };
+            return acc;
+        }, 0)
     }
 };
-const city = 'spb';
-if (cities[city] != undefined && cities[city].temp != undefined) {
-    console.log(cities[city].temp.celsius);
+
+/* товары */
+const car = {
+    id: 1,
+    weight: {
+        kg: 1000
+    },
+    brand: 'Ford'
 };
-console.log(cities[city]?.temp?.celsius);
-// [city]?. такой город есть? - если да -> дальше, если нет -> undefined
-// temp?. температура есть? - если да -> дальше, если нет -> undefined
+const chair = {
+    id: 2,
+    weight: {
+        kg: 2
+    }
+};
+const paper = {
+	id: 3,
+	color: 'red'
+};
+
+console.log(warehouse.addGood(car));
+console.log(warehouse.addGood(car));
+console.log(warehouse.addGood(chair));
+console.log(warehouse.addGood(paper));
+
+console.log(warehouse.findGoodById(2));
+console.log(warehouse.getWeightKg());
