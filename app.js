09ac2578771796c0f9b5b48002e0e02e08a03774
flo0f <144050720+flo0f@users.noreply.github.com>
2023-11-14 15:25:30 +0500

    Ур109 bind

--- app.js
+++ app.js
@@ -2,34 +2,22 @@
 
 const audi = {
    make: 'Audi',
-   model: 'A3',
-   year: 2021,
-   damages: [],
-   addDamage(part, rate) {
-      console.log(`У автомобиля ${this.make} ${this.model} ${this.year} добавлено: \n- повреждение: ${part} со степенью ${rate}`);
-      this.damages.push({
-         part,
-         rate
-      })
-   }
-};
-
-audi.addDamage('Door', 4);
-
-const bmw = {
-   make: 'BMW',
-   model: 'X5',
-   year: 2023,
+   model: 'Q3',
    damages: []
 };
 
-bmw.addDamage = audi.addDamage;
-bmw.addDamage('Bumper', 4);
+const carManipulation = {
+   addDamage(part, rate) {
+      this.damages.push({ part, rate });
+      console.log(`Добавили повреждение на ${this.make} ${this.model}`);
+   }
+};
 
-const addDamageFunction = audi.addDamage;
+// bind
+const addDamageAudi = carManipulation.addDamage.bind(audi);
+addDamageAudi('bumper', 69);
 
-// метод call 
-addDamageFunction.call(audi, 'Backdoor', 3);
+const addDamageAudiRoof = carManipulation.addDamage.bind(audi, 'Roof');
+addDamageAudiRoof(5);
 
-// метод .apply
-addDamageFunction.apply(bmw, ['glass', 5]);
+console.log(audi.damages);
