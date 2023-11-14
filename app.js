39b11c96ecc26db02c51397a97cbec0747fa3915
flo0f <144050720+flo0f@users.noreply.github.com>
2023-11-14 15:13:03 +0500

    Ур108 call, apply

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 'use strict';
 
-const b = 1;
-
-const a = {
-   b,
-   getB: function () {
-      return this.b;
-   },
-   getBAlt() {
-      return this.b;
+const audi = {
+   make: 'Audi',
+   model: 'A3',
+   year: 2021,
+   damages: [],
+   addDamage(part, rate) {
+      console.log(`У автомобиля ${this.make} ${this.model} ${this.year} добавлено: \n- повреждение: ${part} со степенью ${rate}`);
+      this.damages.push({
+         part,
+         rate
+      })
    }
 };
 
-console.log(a.getB());
-console.log(a.getBAlt());
+audi.addDamage('Door', 4);
+
+const bmw = {
+   make: 'BMW',
+   model: 'X5',
+   year: 2023,
+   damages: []
+};
+
+bmw.addDamage = audi.addDamage;
+bmw.addDamage('Bumper', 4);
+
+const addDamageFunction = audi.addDamage;
+
+// метод call 
+addDamageFunction.call(audi, 'Backdoor', 3);
+
+// метод .apply
+addDamageFunction.apply(bmw, ['glass', 5]);
